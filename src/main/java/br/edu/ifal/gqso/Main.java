adc0d2856851c19aec49556a3a4f8e560c143cce
gaabrieljesuss <gjs1@aluno.ifal.edu.br>
2021-04-12 20:42:45 +0000

    Adicionando as classes Banco, BancoTest e Main

---
+++ src/main/java/br/edu/ifal/gqso/Main.java
@@ -0,0 +1,7 @@
+package br.edu.ifal.gqso;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)  {
+        
+    }
+}
---
+++ src/test/java/br/edu/ifal/gqso/BancoTest.java
@@ -0,0 +1,10 @@
+package br.edu.ifal.gqso;
+
+
+
+
+public class BancoTest {
+
+    
+
+}
