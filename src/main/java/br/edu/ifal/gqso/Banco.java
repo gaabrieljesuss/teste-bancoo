d634668a8497fa3c1521a6d297e4012f8ea527e2
gaabrieljesuss <gjs1@aluno.ifal.edu.br>
2021-04-12 20:48:56 +0000

    criação do método saldo e setSaldo na classe Banco

--- src/main/java/br/edu/ifal/gqso/Banco.java
+++ src/main/java/br/edu/ifal/gqso/Banco.java
@@ -1,5 +1,13 @@
 package br.edu.ifal.gqso;
 
 public class Banco {
-
+    private double saldo;
+    
+    public double saldo(){
+        return 0;
+    }
+    
+    public void setSaldo(double saldo){
+         this.saldo = saldo;
+    }
 }
